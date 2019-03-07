34f58d07cb9a2db343382d4aef2c24d4bea61ef2
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-07 19:50:49 -0400

    app gira texto

---
+++ SliderBlaster/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SliderBlaster
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            label_roda.Rotation = slider_roda.Value;
+            int valorLabel = Convert.ToInt32(slider_roda.Value);
+            label_valorDoSlider.Text = slider_roda.Value.ToString();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
